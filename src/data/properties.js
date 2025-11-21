edfbe9b4712dcd62e0472a9e57c30cc88ccb0084
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-11-21 14:32:31 +0000

    fix house4 route

--- src/data/properties.js
+++ src/data/properties.js
@@ -2,7 +2,7 @@ const properties = [
   {id:1,title:'Real House Luxury Villa',location:'Victoria Island, Lagos',bedrooms:6,bathrooms:3,price:3340000000,featured:true,for:'For Sale',rent:false,image:'/house1.png'},
   {id:2,title:'Exquisite Haven Villa',location:'Festac, Lagos',bedrooms:5,bathrooms:3,price:4000000,featured:true,for:'For Rent',rent:true,image:'/house2.jpg'},
   {id:3,title:'Luxe Palatial Villa',location:'Gbagada, Lagos',bedrooms:7,bathrooms:3,price:5350000000,featured:true,for:'For Sale',rent:false,image:'/house3.png'},
-  {id:4,title:'Harmony Luxury Villa',location:'Mushin, Lagos',bedrooms:4,bathrooms:3,price:4000000,featured:true,for:'For Rent',rent:true,image:'/src/assets/house4.png'},
+  {id:4,title:'Harmony Luxury Villa',location:'Mushin, Lagos',bedrooms:4,bathrooms:3,price:4000000,featured:true,for:'For Rent',rent:true,image:'/house4.png'},
   {id:5,title:'Real House Luxury Villa',location:'Victoria Island, Lagos',bedrooms:6,bathrooms:4,price:350000000,featured:true,for:'For Sale',rent:false,image:'/house5.png'},
   {id:6,title:'Real House Luxury Villa',location:'Lekki-Ajah, Lagos',bedrooms:5,bathrooms:3,price:4200000,featured:true,for:'For Rent',rent:true,image:'/house6.png'},
   {id:7,title:'Infinite Bliss Villa',location:'Ishagu, Enugu',bedrooms:5,bathrooms:3,price:2350000000,featured:false,for:'For Sale',rent:false,image:'/house7.png'},
